4f0e7c59073004eb86ece4e23499e13164a39496
GrandChaos <46842959+GrandChaos@users.noreply.github.com>
2021-06-17 08:10:33 +0300

    Add files via upload

---
+++ src/main/java/lessons/LessonsConfiguration.java
@@ -0,0 +1,51 @@
+package lessons;
+
+import lessons.services.CommandManager;
+import lessons.services.GreetingService;
+import lessons.services.GreetingServiceImpl;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+/**
+ * Конфигурационный класс Spring IoC контейнера
+ */
+@Configuration
+@ComponentScan(basePackages = "lessons.services")
+@Import(AnotherConfiguration.class)
+public class LessonsConfiguration {
+
+    @Bean
+    MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Object asyncCommand() {
+        return new Object();
+    }
+
+    @Bean
+    public CommandManager commandManager() {
+        // возвращаем новую анонимную реализацию CommandManager с
+        // с новым объектом Command
+        return new CommandManager() {
+            protected Object createCommand() {
+                return asyncCommand();
+            }
+        };
+    }
+
+//    @Bean
+//    BeanWithDependency beanWithDependency() {
+//        return new BeanWithDependency(greetingService());
+//    }
+
+    @Bean
+    GreetingService greetingService() {
+        return new GreetingServiceImpl();
+    }
+}
